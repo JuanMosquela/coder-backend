19097e1588e5e76286d2c61f655b93f6094c227a
Juan Mosquella <jmosquella11@gmail.com>
2022-08-25 15:29:55 -0300

    entrega para coder

---
+++ archivos/archivos.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+
+
+console.clear()
+class Contenedor {
+    constructor(file){
+        this.file = file
+        this.products = []      
+    }
+
+    save = async (product) => { 
+        try{
+            const id = this.products.length > 0 ? product.id = this.products.length + 1 : product.id = 1       
+            this.products.push(product)
+            await fs.promises.writeFile(`./${this.file}`, JSON.stringify(this.products)) 
+            console.log('El archivo fue creado correctamente')           
+            return id
+
+        }
+        catch{
+            return console.log('Se produjo un error')
+
+        }        
+    }
+
+    getById = (id) => {
+        const productId = this.products.find(product => {
+            return product.id === id
+        })
+        if(productId){
+            console.log(productId)
+            return productId
+        }else{
+            return null
+        }
+    }
+
+    getAll = () => {
+        const auxArray = this.products.map(product => product)
+        console.log(auxArray)
+    }
+
+    deleteById = (id) => {
+        const deleteProduct = this.products.filter(product => {
+            return product.id !== id
+        })
+        if(deleteProduct){            
+            
+            fs.promises.writeFile(`./${this.file}`, JSON.stringify(deleteProduct)).then(data => console.log(this.products))            
+        }    
+
+    }
+
+    deleteAll = () => {
+        fs.unlinkSync(this.file)
+    }    
+    
+}
+
+const producto = new Contenedor('./productos.txt')
+
+
+producto.save({title: 'Mancuerna', price:'2500'})
+producto.save({title: 'Proteina', price:'2300'})
+producto.save({title: 'Barra', price:'4300'})
+
+// producto.getById(3)
+// producto.getAll()
+// producto.deleteAll()
+
+producto.deleteById(1)
+
+// mancuerna.getAll('./productos.txt')
+
